68107956610c3ffd03e59d6d5b4515bd44e630c6
Thiago Luz <thiago.silva@ifg.edu.br>
2025-10-28 18:38:18 -0300

    refactor: update CORS configuration to allow all origins
    - Changed 'allowed_origins' in cors.php from specific localhost URLs to '*' to permit requests from any origin, enhancing flexibility for development and testing environments.

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost', 'http://127.0.0.1:3000', 'http://127.0.0.1'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
